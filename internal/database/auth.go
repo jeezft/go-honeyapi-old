dff9a9811569cb1c347f6e687e46c830176c5fc9
jeezft <74239828+jeezft@users.noreply.github.com>
2023-01-20 18:50:57 +0300

    Database: authorization

---
+++ internal/database/auth.go
@@ -0,0 +1,111 @@
+package database
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/ProSellers/go-honeyapi/internal/database/models"
+	"github.com/ProSellers/go-honeyapi/utils/cfg"
+	"github.com/golang-jwt/jwt"
+)
+
+// func (d *Db) CreateSession(user *models.User) string {
+// 	authKey := jwt.New(jwt.SigningMethodEdDSA)
+// 	claims := authKey.Claims.(jwt.MapClaims)
+
+// 	claims["authorized"] = true
+// 	claims["uID"] = user.ID
+// 	key, err := authKey.SignedString(cfg.Cfg.Jwt.SecretKey)
+// 	if err != nil {
+// 		return ""
+// 	}
+// 	return key
+// }
+
+func (d *Db) CreateSession(user *models.User, ttl time.Duration) (string, error) {
+	authKey := jwt.New(jwt.SigningMethodHS256)
+	claims := authKey.Claims.(jwt.MapClaims)
+
+	tbd := time.Now().Add(ttl)
+
+	claims["authorized"] = true
+	claims["uID"] = user.ID
+	claims["tbd"] = tbd
+
+	key, err := authKey.SignedString([]byte(cfg.Cfg.Jwt.SecretKey))
+	if err != nil {
+		fmt.Println(12123)
+		return "", err
+	}
+	session := models.Session{
+		UserID:     user.ID,
+		SessionKey: key,
+		TBD:        tbd,
+	}
+	tx := d.DB.Create(&session)
+	if tx.Error != nil {
+		fmt.Println(123123)
+		return "", err
+	}
+	return key, nil
+}
+
+// func (d *Db) CheckSession(session string) (*models.Session, error) {
+// 	claims := jwt.MapClaims{}
+// 	_, err := jwt.ParseWithClaims(session, claims, func(token *jwt.Token) (interface{}, error) {
+// 		return []byte(cfg.Cfg.Jwt.SecretKey), nil
+// 	})
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	fmt.Println(claims["uID"])
+// 	fmt.Println(claims["tbd"].(time.Time))
+// 	return nil, nil
+// }
+
+func (d *Db) FindSession(session string) (*models.Session, error) {
+	s := &models.Session{}
+	tx := d.DB.Where(&models.Session{SessionKey: session}).First(s)
+	if tx.Error != nil || tx.RowsAffected < 1 {
+		return nil, errors.New("not found")
+	}
+	return s, nil
+}
+
+func (d *Db) CheckSession(session string) (*models.Session, error) {
+	claims := jwt.MapClaims{}
+	_, err := jwt.ParseWithClaims(session, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(cfg.Cfg.Jwt.SecretKey), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	uID, ok := claims["uID"]
+	if !ok {
+		return nil, fmt.Errorf("claims['uID'] does not exist")
+	}
+
+	tbd, ok := claims["tbd"]
+	if !ok {
+		return nil, fmt.Errorf("claims['tbd'] does not exist")
+	}
+
+	fmt.Println(uID)
+	fmt.Println(tbd)
+
+	s, e := d.FindSession(session)
+
+	return s, e
+}
+
+func (d *Db) FindUserByID(ID uint) (*models.User, error) {
+	u := &models.User{}
+	tx := d.DB.Where("id = ?", ID).First(u)
+	if tx.Error != nil || tx.RowsAffected < 1 {
+		return nil, errors.New("not found")
+	}
+	return u, nil
+}
---
+++ internal/database/users.go
@@ -0,0 +1,39 @@
+package database
+
+import (
+	"errors"
+
+	"github.com/ProSellers/go-honeyapi/internal/database/models"
+)
+
+func (d *Db) createUser(u *models.User) (*models.User, error) {
+	tx := d.DB.Create(u)
+	if tx.RowsAffected < 1 {
+		return nil, errors.New("user already exists")
+	}
+	return u, nil
+}
+
+func (d *Db) CreateUser(username string, password string) (*models.User, error) {
+	u, e := models.User_Frompass(username, password)
+	if e != nil {
+		return nil, e
+	}
+
+	return d.createUser(u)
+}
+
+func (d *Db) FindUser(username string) (*models.User, error) {
+	var user models.User
+
+	tx := d.DB.Where(&models.User{Username: username}).First(&user)
+	if tx.Error != nil || tx.RowsAffected < 1 {
+		return nil, errors.New("not found")
+	}
+
+	return &user, nil
+}
+
+func (d *Db) CheckPassword(user *models.User, password string) error {
+	return models.ComparePass(user.Password, password)
+}
