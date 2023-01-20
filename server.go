c27629b6272f20ba76c63c78a7dc45b60f81d6dc
jeezft <74239828+jeezft@users.noreply.github.com>
2023-01-20 18:48:33 +0300

    feat: Server init functions

--- server.go
+++ server.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/ProSellers/go-honeyapi/handlers"
+	"github.com/ProSellers/go-honeyapi/api/handlers"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -13,6 +13,12 @@ func startServer() error {
 	v1 := api.Group("/v1")
 
 	v1.Get("/hello", handlers.HelloWorld)
+
+	//users
+	u := v1.Group("/users")
+	u.Post("/register", handlers.Register)
+	u.Post("/auth", handlers.Authorization)
+	u.Get("/whoami", handlers.Userinfo)
 
 	return s.Listen(":8080")
 }
